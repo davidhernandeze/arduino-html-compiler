062169e042cbc354fa9847fc2de300de7da04f2d
David H <davidhernandeze@gmail.com>
2024-07-24 11:22:41 -0600

    add uglify to improve compression

--- compiler.js
+++ compiler.js
@@ -1,14 +1,39 @@
 const fs = require('fs');
+const cheerio = require('cheerio');
 const minify = require('html-minifier').minify;
+const UglifyJS = require("uglify-js");
 
-const fileContent = fs.readFileSync('index.html');
-const result = minify(fileContent.toString(), {
+const htmlContent = fs.readFileSync('index.html');
+const $ = cheerio.load(htmlContent);
+
+let scriptContent = '';
+$('script').each((i, elem) => {
+  scriptContent += $(elem).html() + '\n';
+});
+
+const uglyCode = UglifyJS.minify(scriptContent, {
+  parse: { bare_returns: true },
+  mangle: {
+    toplevel: true,
+    properties: true
+  },
+});
+$('script').remove();
+$('body').append(`
+<script>
+  ${uglyCode.code}
+</script>
+`);
+
+const result = minify($.root().html(), {
   removeAttributeQuotes: true,
   minifyCSS: true,
   minifyJS: true,
   removeComments: true,
   collapseWhitespace: true
 });
+
+console.log(result);
 
 const scapeResult = result.replaceAll('"', '\\"')
 const compiledFile = 'String app = "' + scapeResult + '";';
